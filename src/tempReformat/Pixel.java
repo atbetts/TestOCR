1951f8c72badcd6cd77c0f168d6d19ab6ad8310f
atbetts <abetts@ucsd.edu>
2015-08-11 11:43:36 -0700

    Finished pixel core methods doing overhaul of pixelImage. (Might overhaul matrix for generics later but I don't see the merit in doing so.)

--- src/tempReformat/Pixel.java
+++ src/tempReformat/Pixel.java
@@ -1,5 +1,7 @@
 package tempReformat;
 
+import java.awt.*;
+
 /**
  * Created by abetts on 8/11/15.
  */
@@ -8,40 +10,89 @@ public class Pixel {
     private int alpha = 0xFF;
 
     public Pixel(int argb) {
-        alpha = argb >> 24;
-        r = argb >> 16 & 0xFF;
-        g = argb >> 8 & 0xFF;
-        b = argb & 0xFF;
+        setAlpha(alpha = argb >> 24);
+        setRed(argb >> 16 & 0xFF);
+        setGreen(argb >> 8 & 0xFF);
+        setBlue(argb & 0xFF);
     }
 
     public Pixel(int red, int green, int blue) {
-        r = red;
-        b = blue;
-        g = green;
+        setRed(red);
+        setBlue(blue);
+        setGreen(green);
+    }
+
+    public void setColor(int red, int green, int blue) {
+        setRed(red);
+        setBlue(blue);
+        setGreen(green);
+    }
+
+    public void setColor(int red, int green, int blue, int alpha) {
+        setRed(red);
+        setBlue(blue);
+        setGreen(green);
+        setAlpha(alpha);
+    }
+
+    public Color getColor() {
+        return new Color(r, g, b, alpha);
+    }
+
+
+    public boolean setAlpha(int c) {
+        if (c < 0 || c > 255) {
+            return false;
+        }
+        alpha = c;
+        return true;
+    }
+
+
+    public int getARGB() {
+
+        return alpha << 24 + r << 16 + g << 8 + b;
+
+    }
+
+    public int getAlpha() {
+        return alpha;
     }
 
     public int getRed() {
         return r;
     }
 
-    public void setRed(int c) {
+    public boolean setRed(int c) {
+        if (c < 0 || c > 255) {
+            return false;
+        }
         r = c;
+        return true;
     }
 
     public int getGreen() {
         return g;
     }
 
-    public void setGreen(int c) {
+    public boolean setGreen(int c) {
+        if (c < 0 || c > 255) {
+            return false;
+        }
         g = c;
+        return true;
     }
 
     public int getBlue() {
         return b;
     }
 
-    public void setBlue(int c) {
+    public boolean setBlue(int c) {
+        if (c < 0 || c > 255) {
+            return false;
+        }
         b = c;
+        return true;
     }
 
 }
